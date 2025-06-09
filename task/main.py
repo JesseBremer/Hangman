847019784ba8f2adadb5c5313b98a73ad078a9bc
Jesse Bremer <jessesrbremer@gmail.com>
2025-06-09 13:19:23 -0400

    Randomize word choice, guess a letter and return it always lowercase, compare guess to converted word into a list.

--- task/main.py
+++ task/main.py
@@ -1,8 +1,23 @@
+import random as rand
+
 word_list = ["aardvark", "baboon", "camel"]
 
 # TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word. Then print it.
+chosen_word = rand.choice(word_list)
+print(chosen_word)
 
 # TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+guess = input("Guess a letter: ").lower()
+print(guess)
 
 # TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word. Print "Right" if it
 #  is, "Wrong" if it's not.
+
+chosen_word_list = list(chosen_word)
+print(chosen_word_list)
+
+for letter in chosen_word_list:
+    if guess == letter:
+        print('Right')
+    else:
+        print("Wrong")
